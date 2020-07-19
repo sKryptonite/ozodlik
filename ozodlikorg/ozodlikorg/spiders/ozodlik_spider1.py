2cdec8b8fe03e84469da8e266520162c338c8802
sKryptonite <66205023+sKryptonite@users.noreply.github.com>
2020-07-19 14:09:21 +0300

    Add files via upload

---
+++ ozodlikorg/ozodlikorg/spiders/ozodlik_spider1.py
@@ -0,0 +1,31 @@
+import scrapy
+from ..items import OzodlikorgItem
+
+class OzodlikSpider(scrapy.Spider):
+    name = 'ozodlik_1'
+    name2 = input("Search for: ")
+    start_urls = [
+        'https://www.rferl.org/s?k=%s' % name2
+    ]
+
+    def parse(self, response):
+
+        items = OzodlikorgItem()
+
+        all_div = response.css('.fui-grid__inner')
+
+        for media in all_div:
+            title = media.css('.media-block__title--size-3::text').extract()
+            content = media.css('.perex--mb::text').extract()
+            author = media.css('.links__item-link::text').extract()
+
+            items['title'] = title
+            items['content'] = content
+            items['author'] = author
+
+            yield items
+
+        next_page = response.css('li.pagination__item--next a::attr(href)').get()
+        print(next_page)
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)

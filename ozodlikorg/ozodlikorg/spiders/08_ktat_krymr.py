13d7686d5a72ba541d4451d2d85bf7f3640e532b
sKryptonite <66205023+sKryptonite@users.noreply.github.com>
2020-07-20 21:46:35 +0300

    Add files via upload

---
+++ ozodlikorg/ozodlikorg/spiders/08_ktat_krymr.py
@@ -0,0 +1,31 @@
+import scrapy
+from ..items import OzodlikorgItem
+
+class OzodlikSpider(scrapy.Spider):
+    name = 'ktat_krymr'
+    def __init__(self, *a, **kw):
+        super(OzodlikSpider, self).__init__(*a, **kw)
+        self.name2 = kw.get('name2')
+        self.start_urls = ['https://ktat.krymr.com/s?k=%s' % self.name2]
+
+    def parse(self, response):
+
+        items = OzodlikorgItem()
+
+        all_div = response.css('.fui-grid__inner')
+
+        for media in all_div:
+            title = media.css('.media-block__title--size-3::text').extract()
+            content = media.css('.perex--mb::text').extract()
+            author = media.css('.links__item-link::text').extract()
+
+            items['title'] = title
+            items['content'] = content
+            items['author'] = author
+
+            yield items
+
+        next_page = response.css('li.pagination__item--next a::attr(href)').get()
+        print(next_page)
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)

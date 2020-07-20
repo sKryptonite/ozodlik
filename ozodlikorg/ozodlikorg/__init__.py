fb9854268b2eabff60254959de63d7925c12683a
sKryptonite <66205023+sKryptonite@users.noreply.github.com>
2020-07-20 14:45:59 +0300

    Add files via upload

---
+++ ozodlikorg/ozodlikorg/__init__.py
@@ -0,0 +1,16 @@
+# This package will contain the spiders of your Scrapy project
+#
+# Please refer to the documentation for information on how to create and manage
+# your spiders.
+
+
+# class OzodlikSpider(scrapy.Spider):
+#     name = 'rferl'
+#     name2 = input("Search for: ")
+#     start_urls = [
+#         'https://www.rferl.org/s?k=%s' % name2
+#     ]
+#
+#     def __init__(self, *a, **kw):
+#         super(OzodlikSpider, self).__init__(*a, **kw)
+#         self.name2 = kw.get('name2')

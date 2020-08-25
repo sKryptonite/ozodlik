bb4dcc0e12cd04fa74227175fb172378d321f8c1
sKryptonite <ll929025@gmail.com>
2020-08-25 13:34:54 +0300

    new tests

--- ozodlikorg/ozodlikorg/Unitest/unitest.py
+++ ozodlikorg/ozodlikorg/Unitest/unitest.py
@@ -91,7 +91,7 @@ class OzodlikTestCase(unittest.TestCase):
         print("id: " + self.id())
         self.spider.parse = self.str_par
         results = self.spider.parse()
-        self.assertEqual(results, 'This is a monkey functio')
+        self.assertEqual(results, 'This is a monkey functi')
 
 
 if __name__ == '__main__':
